1123610791d2b2aa293e2eb01ab8db3d094c546a
peppizza <spencervess@gmail.com>
2020-04-21 19:56:39 -0400

    added left and right amber buttons and lights and left dispatch button

--- Console/main.py
+++ Console/main.py
@@ -1,64 +1,123 @@
 import RPi.GPIO as gpio
-from time import sleep
 import sys
 import threading
 import random
+import socket
+from time import sleep
+from pprint import pprint
 
 gpio.setmode(gpio.BCM)
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.settimeout(3)
 
-amberlight = 21
-amberbutton = 26
+amberleftlight = 21
+amberleftbutton = 26
+amberrightlight = 12
+amberrightbutton = 6
+greenleftlight = 16
+greenleftbutton = 13
 auto = 19
 man = 20
-blinkAmber = False
+blinkleftAmber = False
+blinkrightAmber = False
+blinkleftGreen = False
 
-gpio.setup(amberbutton, gpio.IN, pull_up_down=gpio.PUD_DOWN)
 gpio.setup(auto, gpio.IN, pull_up_down=gpio.PUD_DOWN)
 gpio.setup(man, gpio.IN, pull_up_down=gpio.PUD_DOWN)
-gpio.setup(amberlight, gpio.OUT)
+gpio.setup(amberleftbutton, gpio.IN, pull_up_down=gpio.PUD_DOWN)
+gpio.setup(amberrightbutton, gpio.IN, pull_up_down=gpio.PUD_DOWN)
+gpio.setup(greenleftbutton, gpio.IN, pull_up_down=gpio.PUD_DOWN)
+gpio.setup(amberleftlight, gpio.OUT)
+gpio.setup(amberrightlight, gpio.OUT)
+gpio.setup(greenleftlight, gpio.OUT)
+server_address = ('172.27.153.71', 15151)
 
 def intro():
     global automan
     while True:
         if gpio.input(auto) == True and gpio.input(man) == False:
             print('starting up')
+            try:
+                s.connect(server_address)
+            except Exception as e:
+                pass
+                # raise e
             print('done!')
             automan = 1
             break
         elif gpio.input(man) == True and gpio.input(auto) == False:
             print('entering manual mode')
+            try:
+                s.connect(server_address)
+            except Exception as e:
+                pass
+                # raise e
             print('done!')
             automan = 0.1
             break
 
-def flashAmber(speed):
-    while blinkAmber == True:
-        gpio.output(amberlight, False)
+def flashleftAmber(speed):
+    while blinkleftAmber == True:
+        gpio.output(amberleftlight, False)
         sleep(speed)
-        gpio.output(amberlight, True)
+        gpio.output(amberleftlight, True)
         sleep(speed)
-    else:
-        gpio.output(amberlight, True)
+
+def flashrightAmber(speed):
+    while blinkrightAmber == True:
+        gpio.output(amberrightlight, False)
+        sleep(speed)
+        gpio.output(amberrightlight, True)
+        sleep(speed)
+
+def flashleftGreen(speed):
+    while blinkleftGreen == True:
+        gpio.output(greenleftlight, False)
+        sleep(speed)
+        gpio.output(greenleftlight, True)
+        sleep(speed)
+        
 
 def main(automan):
-    x = threading.Thread(target=flashAmber, args=(automan, ), daemon=True)
-    global blinkAmber
-    blinkAmber = True
-    x.start()
+    global blinkleftAmber
+    global blinkleftGreen
+    global blinkrightAmber
+    flashleftamber = threading.Thread(target=flashleftAmber, args=(automan, ), daemon=True)
+    flashrightamber = threading.Thread(target=flashrightAmber, args=(automan, ), daemon=True)
+    flashleftgreen = threading.Thread(target=flashleftGreen, args=(automan, ), daemon=True)
+    blinkleftAmber = True
+    blinkrightAmber = True
+    blinkleftGreen = True
+    flashleftamber.start()
+    flashrightamber.start()
+    flashleftgreen.start()
     sleep(0.1)
     while True:
         if gpio.input(auto) == False and gpio.input(man) == False:
-            gpio.output(amberlight, True)
-            blinkAmber = False
-            x.join()
+            gpio.output(amberleftlight, True)
+            gpio.output(amberrightlight, True)
+            gpio.output(greenleftlight, True)
+            blinkleftGreen = False
+            blinkleftAmber = False
+            blinkrightAmber = False
+            flashleftamber.join()
+            flashrightamber.join()
+            flashleftgreen.join()
             gpio.cleanup()
+            s.close()
             sys.exit()
-        if gpio.input(amberbutton) == True:
+        if gpio.input(amberleftbutton) == True:
             print('opening gates..', end='\r')
+        elif gpio.input(amberrightbutton) == True:
+            print('closing gates..', end='\r')
+        if gpio.input(greenleftbutton) == True:
+            print('press both to dispatch', end='\r')
 
 if __name__ == '__main__':
     try:
-        gpio.output(amberlight, True)
+        gpio.output(amberleftlight, True)
+        gpio.output(amberrightlight, True)
+        gpio.output(greenleftlight, True)
         if gpio.input(auto) == True or gpio.input(man) == True:
             print('TURN OFF PANEL BEFORE STARTING')
             gpio.cleanup()
@@ -66,6 +125,9 @@ if __name__ == '__main__':
         intro()
         main(automan)
     except KeyboardInterrupt:
-        blinkAmber = False
+        blinkleftAmber = False
+        blinkrightAmber = False
+        blinkleftGreen = False
         gpio.cleanup()
+        s.close()
         sys.exit()
--- Console/test.py
+++ Console/test.py
@@ -1,17 +1,11 @@
-import RPi.GPIO as gpio
-from time import sleep
-import sys
+import socket
 
-gpio.setmode(gpio.BCM)
-
-gpio.setup(21, gpio.OUT)
-
-try:
-    while True:
-        gpio.output(21, True)
-        sleep(1)
-        gpio.output(21, False)
-        sleep(1)
-except KeyboardInterrupt:
-    gpio.cleanup()
-    sys.exit()
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.settimeout(3)
+s.connect(('172.27.153.71', 15151))
+idle = bytearray([78, 0, 18, 0, 0, 0, 0, 0, 8, 0, 0, 0, 0, 0, 0, 0, 1, 76])
+s.send(idle)
+data = s.recv(1024)
+print(len(data))
+print(data)
+print(data[13])
